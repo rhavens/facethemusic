a6954bdda855dfe1653612d44cd278b4400fd50a
ghagenau <gain.hagenau@viasat.com>
2016-07-17 04:31:03 +0000

    minor changes to Vision.js

--- js/vision.js
+++ js/vision.js
@@ -1,28 +1,31 @@
+
 var gcloud = require('gcloud')({
-  keyFilename: './Auth/service_account.json',
+  keyFilename: './keys/service_account.json',
   projectId: 'instant-gecko-761'
 });
 
 var vision = gcloud.vision();
 
-vision.detectFaces('./manav.PNG', function(err, faces, apiResponse, other){
-    if (err){
-        console.log(err);
-    }
-    console.log(apiResponse.responses[0].faceAnnotations);
+analyseImage('./manav.PNG');
 
-    var faceObj = {
-        "joy": getValue(apiResponse.responses[0].faceAnnotations[0].joyLikelihood),
-        "anger": getValue(apiResponse.responses[0].faceAnnotations[0].angerLikelihood),
-        "sad": getValue(apiResponse.responses[0].faceAnnotations[0].sorrowLikelihood),
-        "surprise": getValue(apiResponse.responses[0].faceAnnotations[0].surpriseLikelihood),
-        "hat": getValue(apiResponse.responses[0].faceAnnotations[0].headwearLikelihood)
-    }
+function analyseImage(image_path){
+    vision.detectFaces(image_path, function(err, faces, apiResponse, other){
+        if (err){
+            console.log(err);
+        }
 
-    console.log(faceObj);
+        var faceObj = {
+            "joy": getValue(apiResponse.responses[0].faceAnnotations[0].joyLikelihood),
+            "anger": getValue(apiResponse.responses[0].faceAnnotations[0].angerLikelihood),
+            "sad": getValue(apiResponse.responses[0].faceAnnotations[0].sorrowLikelihood),
+            "surprise": getValue(apiResponse.responses[0].faceAnnotations[0].surpriseLikelihood),
+            "hat": getValue(apiResponse.responses[0].faceAnnotations[0].headwearLikelihood)
+        }
 
-
-});
+        console.log(faceObj);
+        return faceObj;
+    });
+}
 
 /*
 VERY_UNLIKELY = 5
@@ -32,8 +35,7 @@ UNLIKELY = 2
 VERY_UNLIKELY = 1
 */
 function getValue(likelihood){
-    console.log(likelihood);
-    switch (likelihood){
+    switch (likelihood){    
         case 'VERY_UNLIKELY':
             return 1;
         case 'UNLIKELY':
