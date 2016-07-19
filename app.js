b646babce74c0f0e04f9bf613abf0d43db4c5547
Ryan Havens <ryan.havens@viasat.com>
2016-07-19 21:31:33 +0000

    remove https for heroku

--- app.js
+++ app.js
@@ -372,12 +372,12 @@ app.post('/get_vision_info', function(req, res) {
 //   });
 // });
 
-var httpsServer = https.createServer(credentials, app);
+//var httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(process.env.PORT || 3000, function () {
-      console.log('Listening on port ' + process.env.PORT + '...');
-});
+//httpsServer.listen(process.env.PORT || 3000, function () {
+ //     console.log('Listening on port ' + process.env.PORT + '...');
+//});
   
 
 
-//app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
